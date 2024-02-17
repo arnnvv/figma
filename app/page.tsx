046121694e38d2b27c3cb48c12033f362eb03508
arnnvv <arnav.900@protonmail.com>
2024-02-17 11:53:03 +0530

    cursorConnected

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,7 @@
 "use client";
 
-import { useOthers } from "../liveblocks.config";
+import Live from "@/components/Live";
 
 export default function Page() {
-  const others = useOthers();
-  const userCount = others.length;
-  return (
-    <h1 className="text-2xl text-white">
-      There are {userCount} other user(s) online
-    </h1>
-  );
+  return <Live />;
 }
