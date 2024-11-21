33de3dd652bfb5c3f883c761c5dad1c6126f064f
arnnvv <arnav.900@protonmail.com>
2024-11-21 17:47:30 +0530

    ppr

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "liveblocks.io",
-        port: "",
-      },
-    ],
-  },
-  webpack: (config) => {
-    config.resolve.alias.canvas = false;
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "liveblocks.io",
+				port: "",
+			},
+		],
+	},
+	webpack: (config) => {
+		config.resolve.alias.canvas = false;
 
-    return config;
-  },
-  serverExternalPackages: ["@node-rs/argon2"],
+		return config;
+	},
+	serverExternalPackages: ["@node-rs/argon2"],
+	experimental: {
+		ppr: true,
+	},
 };
 
 export default nextConfig;
