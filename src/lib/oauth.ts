bda35be9f4526b17fef4ee2ac6e84c7cd9fc4583
arnnvv <arnav.900@protonmail.com>
2024-11-30 02:11:40 +0530

    almstdn

--- src/lib/oauth.ts
+++ src/lib/oauth.ts
@@ -1,4 +1,4 @@
-import { Google } from "arctic";
+import { GitHub, Google } from "arctic";
 
 const getGoogleCredentials = () => {
   if (
@@ -26,8 +26,40 @@ const getGoogleCredentials = () => {
   };
 };
 
+const getGithubCredentials = () => {
+  if (
+    !process.env.GITHUB_CLIENT_ID ||
+    process.env.GITHUB_CLIENT_ID.length === 0
+  )
+    throw new Error("GITHUB_CLIENT_ID is missing.");
+
+  if (
+    !process.env.GITHUB_CLIENT_SECRET ||
+    process.env.GITHUB_CLIENT_SECRET.length === 0
+  )
+    throw new Error("GITHUB_CLIENT_SECRET is missing.");
+
+  if (
+    !process.env.GITHUB_REDIRECT_URL ||
+    process.env.GITHUB_REDIRECT_URL.length === 0
+  )
+    throw new Error("GITHUB_REDIRECT_URL is missing.");
+
+  return {
+    clientId: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    redirectURL: process.env.GITHUB_REDIRECT_URL,
+  };
+};
+
 export const google = new Google(
   getGoogleCredentials().clientId,
   getGoogleCredentials().clientSecret,
   getGoogleCredentials().redirectURL,
+);
+
+export const github = new GitHub(
+  getGithubCredentials().clientId,
+  getGithubCredentials().clientSecret,
+  getGithubCredentials().redirectURL,
 );
--- src/lib/user.ts
+++ src/lib/user.ts
@@ -45,3 +45,34 @@ export async function getUserFromGoogleId(
     throw error;
   }
 }
+
+export async function createUserGithub(githubId: number, email: string, username: string): Promise<User> {
+try {
+    const [newUser] = await db
+      .insert(users)
+      .values({
+        username: `github-${username}`,
+        email,
+        password: generateRandomPassword(10),
+        picture: `https://avatars.githubusercontent.com/u/${githubId}`,
+        verified: true,
+      })
+      .returning();
+    return newUser;
+  } catch (error) {
+    if (error instanceof Error && error.message.includes("unique constraint")) {
+      throw new Error("A user with this GitHub ID or email already exists");
+    }
+    throw error;
+  }
+}
+
+export async function getUserFromGitHubId(githubId: number): Promise<User | null> {
+    const foundUsers = await db
+      .select()
+      .from(users)
+      .where(eq(users.picture, `https://avatars.githubusercontent.com/u/${githubId}`))
+      .limit(1);
+
+    return foundUsers.length > 0 ? foundUsers[0] : null;
+}
